7d6aed673d3eb6d569166e0114a3fac4d50388b5
Wirwing <irving.cf@gmail.com>
2014-06-28 00:56:16 -0500

    Plan json reader name refactored.

--- src/main/scala/com/conekta/Plan.scala
+++ src/main/scala/com/conekta/Plan.scala
@@ -6,9 +6,9 @@ import play.api.libs.functional.syntax._
 case class DeletedPlan(id: String, deleted: Boolean)
 
 object DeletedPlan {
-  implicit val cardReads: Reads[DeletedCard] = (
+  implicit val deletedPlanReads: Reads[DeletedPlan] = (
     (__ \ "id").read[String] and
-    (__ \ "deleted").read[Boolean])(DeletedCard.apply _)
+    (__ \ "deleted").read[Boolean])(DeletedPlan.apply _)
 }
 
 case class Plan(
@@ -22,11 +22,17 @@ case class Plan(
   trialPeriodDays: Option[Int],
   currency: String,
   amount: Int,
-  expiryCount: Option[Int]) extends Resource
+  expiryCount: Option[Int]) extends Resource {
+
+  def delete(): DeletedPlan = {
+    request("DELETE", instanceURL(this.id)).as[DeletedPlan]
+  }
+
+}
 
 object Plan extends Resource {
 
-  implicit val cardReads: Reads[Plan] = (
+  implicit val planReads: Reads[Plan] = (
     (__ \ "id").read[String] and
     (__ \ "livemode").read[Boolean] and
     (__ \ "created_at").read[Int] and
@@ -42,5 +48,13 @@ object Plan extends Resource {
   def create(params: Map[String, _]): Plan = {
     return request("POST", classURL, params).as[Plan]
   }
+
+  def where(params: Map[String, _]): List[Plan] = {
+    request("GET", classURL, params).as[List[Plan]]
+  }
+
+  def all(): List[Plan] = this.where(Map.empty)
+
+  def find(id: String): Plan = request("GET", instanceURL(id)).as[Plan]
 
 }
