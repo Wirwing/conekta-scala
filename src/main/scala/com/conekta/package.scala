02233842f281798716c350d258cff12ccb8d1ae4
Wirwing <irving.cf@gmail.com>
2014-06-27 22:23:55 -0500

    Changeset:
    - Custom httpclient added.
    - Conekta object configuration added.

---
+++ src/main/scala/com/conekta/package.scala
@@ -0,0 +1,8 @@
+package com
+
+import org.apache.http.conn.ClientConnectionManager
+
+package object conekta {
+    var apiKey: String = ""
+    var connectionManager: ClientConnectionManager = null
+}
