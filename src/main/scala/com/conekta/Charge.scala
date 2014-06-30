8df360d925986d6cff7cadb82e2032498a6795b9
Wirwing <irving.cf@gmail.com>
2014-06-29 19:38:46 -0500

    Charge model updated.

--- src/main/scala/com/conekta/Charge.scala
+++ src/main/scala/com/conekta/Charge.scala
@@ -66,151 +66,146 @@ object PaymentMethod {
 
   }
 
-  //  implicit val paymentReads: Reads[PaymentMethod] = (__ \ "type").read[String].flatMap[PaymentMethod] {
-  //    
-  ////      (__ \ "x").read[Int].map(CardPayment)
-  //  }
+}
+
+case class Shipment(
+  carrier: Option[String],
+  service: Option[String],
+  trackingId: Option[String],
+  price: Option[Int],
+  address: Option[Address])
+
+object Shipment {
+  implicit val lineItemReads: Reads[Shipment] = (
+    (__ \ "carrier").readNullable[String] and
+    (__ \ "service").readNullable[String] and
+    (__ \ "tracking_id").readNullable[String] and
+    (__ \ "price").readNullable[Int] and
+    (__ \ "state").readNullable[Address])(Shipment.apply _)
+}
+
+case class Refunds(
+  createdAt: Int,
+  amount: Int,
+  currency: String,
+  transaction: String)
+
+object Refunds {
+  implicit val refundReads: Reads[Refunds] = (
+    (__ \ "created_at").read[Int] and
+    (__ \ "amount").read[Int] and
+    (__ \ "currency").read[String] and
+    (__ \ "transaction").read[String])(Refunds.apply _)
+}
+
+case class Address(
+  street1: Option[String],
+  stree2: Option[String],
+  street3: Option[String],
+  city: Option[String],
+  state: Option[String],
+  zip: Option[String],
+  country: Option[String],
+  taxId: Option[String],
+  companyName: Option[String],
+  phone: Option[String],
+  email: Option[String])
+
+object Address {
+  implicit val lineItemReads: Reads[Address] = (
+    (__ \ "street1").readNullable[String] and
+    (__ \ "street2").readNullable[String] and
+    (__ \ "street3").readNullable[String] and
+    (__ \ "city").readNullable[String] and
+    (__ \ "state").readNullable[String] and
+    (__ \ "zip").readNullable[String] and
+    (__ \ "country").readNullable[String] and
+    (__ \ "tax_id").readNullable[String] and
+    (__ \ "company_name").readNullable[String] and
+    (__ \ "phone").readNullable[String] and
+    (__ \ "email").readNullable[String])(Address.apply _)
+
+}
+
+case class LineItem(
+  name: Option[String],
+  sku: Option[String],
+  unitPrice: Option[Int],
+  description: Option[String],
+  quantity: Option[Int],
+  itemType: Option[Int],
+  category: Option[String])
+  
+object LineItem {
+  implicit val itemReads: Reads[LineItem] = (
+    (__ \ "name").readNullable[String] and
+    (__ \ "sku").readNullable[String] and
+    (__ \ "unit_price").readNullable[Int] and
+    (__ \ "description").readNullable[String] and
+    (__ \ "quantity").readNullable[Int] and
+    (__ \ "type").readNullable[Int] and
+    (__ \ "category").readNullable[String])(LineItem.apply _)
+
+}
+
+case class Details(
+  name: Option[String],
+  phone: Option[String],
+  email: Option[String],
+  dateOfBirth: Option[String],
+  lineItems: Option[List[LineItem]],
+  address: Option[Address],
+  shipment: Option[Shipment])
+
+object Details {
+  implicit val detailReads: Reads[Details] = (
+    (__ \ "name").readNullable[String] and
+    (__ \ "phone").readNullable[String] and
+    (__ \ "email").readNullable[String] and
+    (__ \ "date_of_birth").readNullable[String] and
+    (__ \ "line_items").readNullable[List[LineItem]] and
+    (__ \ "address").readNullable[Address] and
+    (__ \ "shipment").readNullable[Shipment])(Details.apply _)
 
 }
 
 case class Charge(
+
   id: String,
-  liveMode: Boolean,
   createdAt: Int,
-  status: String,
-  paymentMethod: PaymentMethod) extends Resource
+  status: Int,
+  currency: String,
+  description: Option[String],
+  referenceId: String,
+  failureCode: Option[String],
+  failureMessage: Option[String],
+  amount: Int,
+  paymentMethod: PaymentMethod,
+  details: Details,
+  fee: Int,
+  monthlyInstallments: Option[Int],
+  refunds: Option[Refunds]) extends Resource
 
 object Charge extends Resource {
 
   implicit val chargeReads: Reads[Charge] = (
     (__ \ "id").read[String] and
-    (__ \ "livemode").read[Boolean] and
     (__ \ "created_at").read[Int] and
-    (__ \ "status").read[String] and
-    (__ \ "payment_method").read[PaymentMethod])(Charge.apply _)
+    (__ \ "status").read[Int] and
+    (__ \ "currency").read[String] and
+    (__ \ "description").readNullable[String] and
+    (__ \ "reference_id").read[String] and
+    (__ \ "failure_code").readNullable[String] and
+    (__ \ "failure_message").readNullable[String] and
+    (__ \ "amount").read[Int] and
+    (__ \ "payment_method").read[PaymentMethod] and
+    (__ \ "details").read[Details] and
+    (__ \ "fee").read[Int] and
+    (__ \ "monthly_installments").readNullable[Int] and
+    (__ \ "refunds").readNullable[Refunds])(Charge.apply _)
 
   def find(id: String): Charge = request("GET", instanceURL(id)).as[Charge]
 
   def all(): List[Charge] = request("GET", classURL).as[List[Charge]]
 
 }
-  
-//case class Shipment(
-//  carrier: Option[String],
-//  service: Option[String],
-//  trackingId: Option[String],
-//  price: Option[Int],
-//  address: Option[Address])
-//
-//object Shipment {
-//  implicit val lineItemReads: Reads[Shipment] = (
-//    (__ \ "carrier").readNullable[String] and
-//    (__ \ "service").readNullable[String] and
-//    (__ \ "tracking_id").readNullable[String] and
-//    (__ \ "price").readNullable[Int] and
-//    (__ \ "state").readNullable[Address])(Shipment.apply _)
-//}
-//
-//case class Refunds(
-//  createdAt: Int,
-//  amount: Int,
-//  currency: String,
-//  transaction: String)
-//
-//object Refunds {
-//  implicit val refundReads: Reads[Refunds] = (
-//    (__ \ "created_at").read[Int] and
-//    (__ \ "amount").read[Int] and
-//    (__ \ "currency").read[String] and
-//    (__ \ "transaction").read[String])(Refunds.apply _)
-//}
-//
-//case class Address(
-//  street1: Option[String],
-//  stree2: Option[String],
-//  street3: Option[String],
-//  city: Option[String],
-//  state: Option[String],
-//  zip: Option[String],
-//  country: Option[String],
-//  taxId: Option[String],
-//  companyName: Option[String],
-//  phone: Option[String],
-//  email: Option[String])
-//
-//object Address {
-//  implicit val lineItemReads: Reads[Address] = (
-//    (__ \ "street1").readNullable[String] and
-//    (__ \ "street2").readNullable[String] and
-//    (__ \ "street3").readNullable[String] and
-//    (__ \ "city").readNullable[String] and
-//    (__ \ "state").readNullable[String] and
-//    (__ \ "zip").readNullable[String] and
-//    (__ \ "country").readNullable[String] and
-//    (__ \ "tax_id").readNullable[String] and
-//    (__ \ "company_name").readNullable[String] and
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//    
-//    
-//      //  implicit val paymentReads: Reads[PaymentMethod] = (
-//  //    (__ \ "type").as[String] match {
-//  //      case "card_payment" => _.as[CardPayment]
-//  //      case "cash_payment" => _.as[OxxoPayment]
-//  //      case "bank_transfer_payment" => _.as[BankTransferPayment]
-//  //    })
-//
-//  implicit val paymentReads: Reads[PaymentMethod] = {
-//    
-//    
-//    ((__ \ "type").read[String])((paymentType: String) => {
-//      
-//      paymentType match{
-//        case "bank_transfer_payment" => {
-//          
-//        }
-//      }
-//      
-//    })
-//     
-//    
-//    null
-////      case "bank_transfer_payment" => {
-////      
-////      (__ \ "service_name").read[String] and
-////    (__ \ "service_number").read[String] and
-////    (__ \ "reference").read[String])((serviceName: String, serviceNumber: String, reference: String ) => BankTransferPayment(serviceName, serviceNumber, reference))
-//      
-//    
-//      
-//  }
-//    
-//      
-////      (__ \ "x").read[Int].map(Bar)
-//      
-////      
-////      (__ \ "brand").read[String] and
-////    (__ \ "auth_code").read[String] and
-////    (__ \ "last4").read[String] and
-////    (__ \ "exp_month").read[String] and
-////    (__ \ "exp_year").read[String] and
-////    (__ \ "name").read[String])((yes: Int, no: Int, maybe: Int) => new Vote(yes, no, maybe)) 
-//      
-////    }
-////    case "Baz" => (__ \ "s").read[String].map(Baz)
-////    case "Bah" => (__ \ "s").read[String].map(Bah)
-//  }
